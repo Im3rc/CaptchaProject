600339606530f5f98d7d90cb521040537c03af02
Im3rc <71458376+Im3rc@users.noreply.github.com>
2021-05-05 23:14:05 +0530

    Update CaptchaVerifier.java

--- ServeltCode/CaptchaVerifier.java
+++ ServeltCode/CaptchaVerifier.java
@@ -2,14 +2,12 @@ package com.CaptchaProject.app;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
-
 /**
  * Servlet implementation class CaptchaVerifier
  */
@@ -24,33 +22,22 @@ public class CaptchaVerifier extends HttpServlet {
             throws ServletException, IOException {
     	   response.setContentType("text/html");  
            PrintWriter out=response.getWriter();  
-        //   request.getRequestDispatcher("/Form/CaptchaScript.jsp").include(request, response);  
+  
  String clientCheckcode = request.getParameter("validateCode");
- System.out.println(clientCheckcode);
- String serverCheckcode = (String) request.getSession().getAttribute("checkcode");
- System.out.println(serverCheckcode);
+
+ String serverCheckCode = DataStorage.getDataMap().get("key").toString();
+ out.print(serverCheckCode);  
  String  n=request.getParameter("Name");
- if (clientCheckcode.equals(serverCheckcode)) {
-//     out.println ("<script>");
-//     out.println ("window.close()");
+ if (clientCheckcode.equals(serverCheckCode)) {
 
 	 request.setAttribute("name", n);
-//     out.println ("</script>");
 	 request.getRequestDispatcher("/Form/Welcome.jsp").forward(request, response);
 	  
 	 System.out.println("Verification Code Verification!");
  }else {
-	 request.setAttribute("errorMessage", "Invalid CAPTCHA!! Please Try Again ");
-//     out.print("Incorrect CAPTCHA!! "
-//     		+ "Please Try Again");  
+	 request.setAttribute("errorMessage", "Invalid CAPTCHA!! Please Try Again "); 
      request.getRequestDispatcher("/Form/Index.jsp").include(request, response);  
-
-//response.getWriter().println("foo");
-//request.getRequestDispatcher("CaptchaScript.jsp").forward(request, response);
-//response.getWriter().println("foo");
-	
  }
  out.close();
+	}
 }
- }
- 
